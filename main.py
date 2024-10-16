d38a0f9fdbba3dc33faf68b35b5cdb458e7778a5
maxrice <mxrice@loyola.edu>
2024-10-15 20:52:15 -0400

    fixed code

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-import math
-def ski_jump_calculator():
-    hill_type = input("Enter hill type (normal/large): ").strip().lower()
+#Max Rice and Jenna Cuti
+#Lab 2
+#Program takes a ski jump and based on the hill type calculates air time, distance traveled, and points scored
 
-    if hill_type == "normal":
+
+
+import math
+
+#input from user hill type
+hill_type = input("Enter hill type (normal/large): ").strip().lower()
+
+#stores info for different hill types
+if hill_type == "normal":
         speed = 46
         points_per_meter = 2
         par = 90
         height = 60
-    elif hill_type == "large":
+elif hill_type == "large":
         speed = 70
         points_per_meter = 1.8
         par = 120
         height = 100
-    else:
+else:
         print("Invalid hill type.")
-        return
 
-    time_in_air = math.sqrt((2 * height) / 9.8)
+#variables which calculate the outputs
+time_in_air = math.sqrt((2 * height) / 9.8)
 
-    distance_traveled = speed * time_in_air
+distance_traveled = speed * time_in_air
 
-    points = 60 + (distance_traveled - par) * points_per_meter
+points = 60 + (distance_traveled - par) * points_per_meter
 
-    print(f"The Olympic ski jumper traveled {distance_traveled:.2f} meters.")
-    print(f"Points earned: {points:.2f}")
+print(f"The Olympic ski jumper traveled {distance_traveled:.2f} meters.")
+print(f"Points earned: {points:.2f}")
 
-    if points >= 61:
-        print("Great job for doing better than par!")
-    elif points < 10:
-        print("What happened??")
-    else:
-        print("Sorry you didn’t go very far.")
+#if statements which tell user how good they do
+if points >= 61:
+    print("Great job for doing better than par!")
+elif points < 10:
+    print("What happened??")
+else:
+    print("Sorry you didn’t go very far.")
 
